85a3ecbdb275d2eeb082e3ba89484565848ac0a2
whasnat <w.hasnat94@gmail.com>
2022-01-18 21:28:48 +0600

    0.0.1 - Added user entity and UserPostest

---
+++ src/main/java/com/rm/rm_universe/user/User.java
@@ -0,0 +1,8 @@
+package com.rm.rm_universe.user;
+
+public class User {
+    private String username;
+    private String displayName;
+    private String password;
+    
+}
---
+++ src/test/java/com/rm/rm_universe/UserControllerTest.java
@@ -0,0 +1,17 @@
+package com.rm.rm_universe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class UserControllerTest {
+    @Test
+    public void postUser_whenUserIsValid(){
+
+    }
+}
