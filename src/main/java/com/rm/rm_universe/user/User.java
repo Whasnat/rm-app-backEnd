d07467f5ea10abbd59abc449bc38401c85f95fa2
whasnat <w.hasnat94@gmail.com>
2022-01-18 21:51:57 +0600

    0.0.2 - api/1.0/users init (rm/src/App.css showing Error:(2, 22) Unknown at-rule)

--- src/main/java/com/rm/rm_universe/user/User.java
+++ src/main/java/com/rm/rm_universe/user/User.java
@@ -1,8 +1,11 @@
 package com.rm.rm_universe.user;
 
+import lombok.Data;
+
+@Data
 public class User {
     private String username;
     private String displayName;
     private String password;
-    
+
 }
