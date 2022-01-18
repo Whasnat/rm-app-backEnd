f437d016a31c40adad5a3b0757f6b095fad2db45
whasnat <w.hasnat94@gmail.com>
2022-01-18 21:05:58 +0600

    rm_init

---
+++ src/main/java/com/rm/rm_universe/RmUniverseApplication.java
@@ -0,0 +1,13 @@
+package com.rm.rm_universe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmUniverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmUniverseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rm/rm_universe/RmUniverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rm.rm_universe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmUniverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
