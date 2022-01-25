7f2c9f043e2fb7b90ffc95e36f549cbe096dd7bc
whasnat <w.hasnat94@gmail.com>
2022-01-26 02:16:48 +0600

    0.0.9 - passwordhash with test

--- src/test/java/com/rm/rm_universe/UserControllerTest.java
+++ src/test/java/com/rm/rm_universe/UserControllerTest.java
@@ -16,6 +16,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
@@ -61,6 +63,14 @@ public class UserControllerTest {
         assertThat(response.getBody().getMessage()).isNotNull();
     }
 
+    @Test
+    public void postUser_whenUserIsValid_hashPassword(){
+        User user = createValidUser();
+        testRestTemplate.postForEntity(API_1_O_USERS,user, Object.class);
+        List<User> users = userRepository.findAll();
+        User inDB = users.get(0);
+        assertThat(inDB.getPassword()).isNotEqualTo(user.getPassword());
+    }
     @Test
     public void postUser_whenUserIsValid_userSavedToDatabase() {
         User user = createValidUser();
